dbb024726e67bdbae7ccb3ee1246ff474ee6df4e
Bucsy Benjamin <bucsy.benjamin@simonyi.bme.hu>
2024-04-13 06:52:16 +0200

    fix collision

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,11 @@ public class Play : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
     {
         transform.position += new Vector3(
             Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime,
