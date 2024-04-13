14337933971cdbe7d85ce4fef59aebe1c31fcb50
bertalankovacs <bertalan.kovacs@ivm-vending.com>
2024-04-13 06:17:32 +0200

    Assets and moving circle

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+
+    public float speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(
+            Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime,
+            Input.GetAxisRaw("Vertical") * speed * Time.deltaTime
+        );
+    }
+}
